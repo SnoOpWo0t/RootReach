d578a2da318ad314237d4cd6119b44f79ac24f6a
Afrin Sultana Akhi <afrinsultanaakhi138@gmail.com>
2024-11-18 23:20:26 +0600

    base html

--- RootReach/urls.py
+++ RootReach/urls.py
@@ -23,4 +23,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 path('', views.home, name='home'),
 path('product/',include('products.urls')),
+                  path('about/', views.about, name='about'),
+
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- products/models.py
+++ products/models.py
@@ -17,6 +17,9 @@ class Product(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE, default=1)
     description = models.TextField()
     image = models.ImageField(upload_to='uploads/products/')
+    #is_sale
+    is_sale = models.BooleanField(default=False)
+    sale_price = models.FloatField(default=0)
     def __str__(self):
         return self.name
 
