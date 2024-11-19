784ebf3f22c4476117e2cc2328664ad51883335a
Afrin Sultana Akhi <afrinsultanaakhi138@gmail.com>
2024-11-19 09:25:24 +0600

    admin login done

--- RootReach/urls.py
+++ RootReach/urls.py
@@ -23,6 +23,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 path('', views.home, name='home'),
 path('product/',include('products.urls')),
-                  path('about/', views.about, name='about'),
+path('about/', views.about, name='about'),
+
+path('login/', views.login_user, name='login'),
+path('logout/', views.logout_user, name='logout'),
 
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- RootReach/views.py
+++ RootReach/views.py
@@ -1,9 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from products.models import Product
-
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 def home(request):
     products = Product.objects.all()
     return render(request, 'home.html',{'products':products})
 def about(request):
-
     return render(request, 'about.html',{})
+
+
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, 'You are now logged in')
+            return redirect('home')
+        else:
+             messages.success(request, 'there was an error')
+        return redirect('login')
+    else:
+
+        return render(request, 'login.html')
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, 'You have been logged out.')
+    return redirect('home')
